dc8e29f1e510f2d9133990d71015ea2bb82ec2a7
Administrator <robex.0933509060@gmail.com>
2024-08-25 17:29:07 +0300

    Урок из тернарного оператора

--- kursJava/src/Main.java
+++ kursJava/src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
+public class Man {
+
+    private int age;
+
+    public Man(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public static void main(String[] args) {
-    System.out.println("Hello Git Hab");
+
+        Man man = new Man(22);
+
+        String securityAnswer;
+
+        if (man.getAge() >= 18) {
+            securityAnswer = "Все в порядке, проходите!";
+        } else {
+            securityAnswer = "Этот фильм не подходит для вашего возраста!";
+        }
+
+        System.out.println(securityAnswer);
+
     }
 }
